54d0bd2b69ffb7ab7dc37720ca371c9216a1ea71
Nguyen Dang Kiet <kiet.nguyen-dang@internal.banvien.com>
2025-02-26 17:26:42 +0700

    feat(utils): impl date time util

---
+++ src/main/java/dev/ngdangkietswe/swejavacommonshared/constants/CommonConstant.java
@@ -0,0 +1,11 @@
+package dev.ngdangkietswe.swejavacommonshared.constants;
+
+/**
+ * @author ngdangkietswe
+ * @since 2/26/2025
+ */
+
+public class CommonConstant {
+
+    public static final String YYYY_MM_DD_REGEX = "^\\d{4}-\\d{2}-\\d{2}$";
+}
---
+++ src/main/java/dev/ngdangkietswe/swejavacommonshared/utils/DateTimeUtil.java
@@ -0,0 +1,57 @@
+package dev.ngdangkietswe.swejavacommonshared.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+/**
+ * @author ngdangkietswe
+ * @since 2/26/2025
+ */
+
+public class DateTimeUtil {
+
+    public static final String YYYY_MM_DD_PATTERN = "yyyy-MM-dd";
+    public static final String YYYY_MM_DD_HH_MM_SS_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static final String HH_MM_SS_PATTERN = "HH:mm:ss";
+
+    public static DateTimeFormatter createDateTimeFormatter(String pattern) {
+        return DateTimeFormatter.ofPattern(pattern);
+    }
+
+    public static ZoneId getZoneId(String timezone) {
+        return StringUtils.isEmpty(timezone) ? getDefaultZoneId() : ZoneId.of(timezone);
+    }
+
+    public static ZoneId getDefaultZoneId() {
+        return ZoneId.systemDefault();
+    }
+
+    public static String timestampAsStringWithDefaultZone(Timestamp timestamp) {
+        return timestampAsStringWithDefaultZone(timestamp, HH_MM_SS_PATTERN);
+    }
+
+    public static String timestampAsStringWithDefaultZone(Timestamp timestamp, String pattern) {
+        return timestamp.toInstant().atZone(getDefaultZoneId())
+                .format(createDateTimeFormatter(pattern));
+    }
+
+    public static String timestampAsStringWithDefaultZoneAndFormat(Timestamp timestamp) {
+        return timestampAsStringWithDefaultZone(timestamp, HH_MM_SS_PATTERN);
+    }
+
+    public static String timestampAsString(Timestamp timestamp, ZoneId zoneId) {
+        return timestampAsString(timestamp, HH_MM_SS_PATTERN, zoneId);
+    }
+
+    public static String timestampAsString(Timestamp timestamp, String pattern, ZoneId zoneId) {
+        return Objects.nonNull(timestamp)
+                ? timestamp.toInstant().atZone(zoneId).format(createDateTimeFormatter(pattern))
+                : EMPTY;
+    }
+}
